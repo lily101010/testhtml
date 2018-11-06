735f9a7b72f08db55fc1b99a4ccfcfb15d9c4d7a
lily101010 <43978750+lily101010@users.noreply.github.com>
2018-11-06 12:00:07 +0800

    333
    333

--- sketch2.js
+++ sketch2.js
@@ -11,6 +11,22 @@ KNN Image Classifier example with p5.js
 
 let knn;
 let video;
+const btn = document.getElementById("buttonNew");
+const btn = document.getElementById("countdown");
+var countdown_num = 5;
+
+function countdown_f() {
+	cdn.innerHTML = countdown_num;
+	if(countdown_num == 0){
+		cdn.style.display = "none";
+		predict();
+		clearTimeout(ct);
+		
+	}else{
+		countdown_num--;
+		ct = setTimeout(countdown_f,1000);
+	}
+}
 
 function setup() {
   noCanvas();
@@ -32,7 +48,8 @@ function createButtons() {
 
 
 function restart() {
-
+   countdown_f();
+   btn.style.display = "none";
    predict();
 
 }
@@ -69,13 +86,24 @@ function gotResults(results) {
   }
   //select('#result').html(msg);
 
-  setTimeout(function(){
+  win_t = setTimeout(win,3000);
+  
+  /*setTimeout(function(){
     predict();
-  }, 50);
+  }, 50);*/
 }
 
 // Clear the data in one class
 function clearClass(classIndex) {
   knn.clearClass(classIndex);
 }
+
+function win(){
+	cdn.innerHTML = "WIN";
+	cdn.style.display = "";
+	countdown_num = 5;
+	document.getElementById("myImg").src = "";
+	btn.style.display = "";
+}
+
 
